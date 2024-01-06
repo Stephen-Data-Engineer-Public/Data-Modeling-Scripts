667fc2b6d612ab31cadc72d72d55d3cabf4efc5d
stephen nwoye <resgad.inc@gmail.com>
2024-01-06 00:32:21 +0000

    This creates the tables for the shopping cart data model

---
+++ Shopping_cart/shopping_cart_Table_script.sql
@@ -0,0 +1,27 @@
+-- Table for items by cart
+CREATE TABLE dbo.items_by_cart 
+(
+    cart_id                     VARCHAR(64)    NOT NULL,
+    timestamp_                  TIME       NOT NULL, 
+    item_id                     VARCHAR         NOT NULL,
+    item_name                   VARCHAR         NOT NULL,
+    item_description            VARCHAR         NOT NULL,
+    item_price                  DECIMAL         NOT NULL,
+    quantity                    INT             NOT NULL,
+    cart_subtotal               DECIMAL         NOT NULL
+)
+WITH
+(
+                DISTRIBUTION = HASH(cart_id),
+                CLUSTERED COLUMNSTORE INDEX
+
+);
+-- Table for items by name
+CREATE TABLE dbo.items_by_name
+(
+    id                     VARCHAR(32)     NOT NULL,
+    name                   VARCHAR         NOT NULL,
+    description            VARCHAR         NOT NULL,
+    price                  DECIMAL         NOT NULL
+);
+-- Table for carts by user
